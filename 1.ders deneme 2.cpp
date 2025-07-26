3f95e64c51deea0cb0a79cfe26f3286d7bdd74aa
Mirac Bozaci <mbozacioglu06@gmail.com>
2025-07-26 11:24:44 +0300

    Update 1.ders deneme 2.cpp

--- 1.ders deneme 2.cpp
+++ 1.ders deneme 2.cpp
@@ -5,16 +5,18 @@ int main()
 {
 	
 	
-	int a,b,c,d;
+	int a,b,c,d,f,g;
 	
 	a=1,
 	b=2,
 	c=3,
 	d=4;
+	f=5;
+	g=6;
 	
 	
 	
-	printf("toplam=%d",a+b+c+d);
+	printf("toplam=%d",a+b+c+d+f+g);
 	
 	
 	
